041963736e1a4d057467f63113c192c5240ed1f6
Yash Soni <39760283+yashsoni27@users.noreply.github.com>
2024-12-17 13:22:52 +0000

    deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output: "export",
+  // distDir: "dist",
 };
 
 export default nextConfig;
